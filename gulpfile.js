991d95f646155cb9fec203403b71b7d0d485c4b4
Alexander Mortensen <alextmortensen@gmail.com>
2017-05-17 22:05:06 -0600

    commit that

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+		uglify = require('gulp-uglify'),
+		sass = require('gulp-sass'),
+		sourcemaps = require('gulp-sourcemaps'),
+		plumber = require('gulp-plumber'),
+		babel = require('gulp-babel'),
+		browserSync = require('browser-sync'),
+		nodemon = require('gulp-nodemon'),
+		concat = require('gulp-concat'),
+		autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('scripts', function(){
+	gulp.src('public/js/*.js')
+	.pipe(plumber())
+	.pipe(babel({presets: ['es2015']}))
+	.pipe(uglify())
+	.pipe(concat('scripts.min.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('public/build'))
+	.pipe(browserSync.stream())
+})
+
+gulp.task('styles', function(){
+	gulp.src('public/styles/**/*.scss')
+	.pipe(plumber())
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}))
+	.pipe(concat('css.min.css'))
+	.pipe(autoprefixer())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('public/build'))
+	.pipe(browserSync.stream())
+})
+
+gulp.task('browser-sync-init', function(){
+  browserSync.init({
+		open: false,
+    proxy: 'http://localhost:5000'
+  })
+})
+
+gulp.task('bs-reload', function(){
+  browserSync.reload()
+})
+
+gulp.task('default', function(cb){
+	let started = false
+  nodemon({
+    script: 'server.js'
+  }).on('start', function (){
+  	if(!started){
+  		gulp.start('browser-sync')
+  		cb()
+  		started = true
+  	}
+  })
+})
+
+gulp.task('browser-sync', ['scripts', 'styles', 'browser-sync-init'], function(){
+	gulp.watch('public/js/**/*.js', ['scripts'])
+	gulp.watch('public/styles/**/*.scss', ['styles'])
+	gulp.watch('public/**/*.html', ['bs-reload'])
+})
---
+++ public/js/app.js
@@ -0,0 +1,60 @@
+const app = angular.module('portfolio', [])
+
+app.directive('menu', function(){
+	return {
+		templateUrl: '../directives/menu.html',
+		restrict: 'E',
+		replace: true
+	}
+})
+
+app.directive('cover', function(){
+	return {
+		templateUrl: '../directives/cover.html',
+		restrict: 'E',
+		replace: true
+	}
+})
+
+app.directive('summary', function(){
+	return {
+		templateUrl: '../directives/summary.html',
+		restrict: 'E',
+		replace: true
+	}
+})
+
+app.directive('skills', function(){
+	return {
+		templateUrl: '../directives/skills.html',
+		restrict: 'E',
+		replace: true
+	}
+})
+
+app.directive('projects', function(){
+	return {
+		templateUrl: '../directives/projects.html',
+		restrict: 'E',
+		replace: true
+	}
+})
+
+app.directive('contact', function(){
+	return {
+		templateUrl: '../directives/contact.html',
+		restrict: 'E',
+		replace: true,
+		link: () => {
+			smoothScroll.init({
+				speed: 1000,
+				easing: 'easeInOutQuint'
+			})
+
+			gumshoe.init()
+
+			new Rellax('.rellax')
+		}
+	}
+})
+

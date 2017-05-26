9143fd395edb7a8d9cd3a787a4e2699f55ee417a
Alexander Mortensen <alextmortensen@gmail.com>
2017-05-25 19:25:58 -0600

    changes server port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.listen(5000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log('Server started! port 5000')
 })
